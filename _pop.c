42979f0055b66a665229351644f73eb890e37cd4
Habtamu <habtamuwolde96@gmail.com>
2023-09-21 03:43:07 -0700

    pop

---
+++ _pop.c
@@ -0,0 +1,30 @@
+#include "monty.h"
+
+/**
+* pop - Remove the top element from the stack.
+* @node: Pointer to the head of the stack.
+* @line_count: Current line number.
+*
+* Return: void
+*/
+
+void pop(stack_t **node, unsigned int line_count)
+{
+	if (node == NULL || *node == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_count);
+		_free(*node);
+		exit(EXIT_FAILURE);
+	}
+
+	stack_t *top = *node;
+	*node = top->next;
+
+	if (*node != NULL)
+	{
+		(*node)->prev = NULL;
+	}
+
+	free(top);
+}
+
